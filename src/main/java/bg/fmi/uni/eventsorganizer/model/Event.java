af380d57e8ac33d58a65d48e599a01c4c3effaaf
amadjarova <a_madjarova@abv.bg>
2025-05-11 20:46:39 +0300

    Add project structure

---
+++ src/main/java/bg/fmi/uni/eventsorganizer/model/Event.java
@@ -0,0 +1,65 @@
+package bg.fmi.uni.eventsorganizer.model;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private static int eventIdCounter = 1;
+
+    private int id;
+    private String title;
+    private String description;
+    private String category;
+    private String location;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String rules;
+    private int organizerId;
+    private int sponsorId;
+
+    public Event(String title, String description, String category, String location,
+                 LocalDateTime startDate, LocalDateTime endDate, String rules,
+                 int organizerId, int sponsorId) {
+        this.id = eventIdCounter++;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rules = rules;
+        this.organizerId = organizerId;
+        this.sponsorId = sponsorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public String getRules() {
+        return rules;
+    }
+}
