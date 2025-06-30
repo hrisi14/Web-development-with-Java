eed526397a3b54b802517854a7b87b0dad8eb11b
hrisi14 <hristinafrangova.hf@gmail.com>
2025-06-30 16:35:17 +0300

    Added event-review and event-edit forms

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EventsCatalogueComponent } from './events-catalogue/events-catalogue.component';
 import { CreateEventComponent } from './create-event/create-event.component';
+import { EventReviewComponent } from './event-review/event-review.component';
+import {EventEditComponent } from './event-edit/event-edit.component';
 
 export const routes: Routes = [
 { path: 'register', component: RegisterComponent },
 { path: '', redirectTo: 'register', pathMatch: 'full' },
 { path: 'login', component: LoginComponent },
 { path: 'events-catalogue', component: EventsCatalogueComponent },
-{ path: 'create-event', component: CreateEventComponent }
+{ path: 'create-event', component: CreateEventComponent },
+{ path: 'review-event', component: EventReviewComponent },
+{ path: 'edit-event', component: EventEditComponent },
 ];
